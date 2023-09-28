928222cf7ddb7062060e8f164f7e913571798dfc
WilliamNW <williamnw943@gmail.com>
2023-09-28 14:21:31 +0700

    navbar move to bottom feature

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Progress Scroll Bar
-const scrollInnerProgressBar = document.querySelector(
-  "div.scroll-progress div.inner-progress"
-);
-
 document.addEventListener("scroll", (e) => {
   const currentScroll = Math.ceil(window.scrollY);
+  const scrollInnerProgressBar = document.querySelector(
+    "div.scroll-progress div.inner-progress"
+  );
   const endScroll = document.documentElement.scrollHeight - innerHeight;
   const scrollProgress = (currentScroll / endScroll) * 100;
+  const navbar = document.querySelector("nav.navbar");
+  const navbarHeight = navbar.getBoundingClientRect().height;
 
+  // Progress Scroll Bar
   scrollInnerProgressBar.style.width = `${scrollProgress}%`;
+
+  // Navbar move to bottom
+  if (currentScroll > navbarHeight) {
+    navbar.classList.add("moved-bottom");
+  } else {
+    navbar.classList.remove("moved-bottom");
+  }
 });
